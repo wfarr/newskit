8f3d871fd26d4329e00931c17e5fec06f6d8a380
Ethan Osten <eosten@Phineas.(none)>
2008-06-12 16:06:24 -0700

    add a show/hide entry to the StatusIcon menu

--- src/Summa/Summa.Gui/StatusIcon.cs
+++ src/Summa/Summa.Gui/StatusIcon.cs
@@ -46,51 +46,61 @@ namespace Summa {
                 PopupMenu += RightClickMenu;
             }
             
-             private void ToggleBrowserStatus(object obj, EventArgs args) {
-                 if ( shown ) {
-                     b.Hide();
-                     shown = false;
+            private void ToggleBrowserStatus(object obj, EventArgs args) {
+                if ( shown ) {
+                    b.Hide();
+                    shown = false;
                 } else {
                     b.Show();
                     shown = true;
                 }
-             }
+            }
 
-             private void RightClickMenu(object obj, EventArgs args) {
-                 Gtk.Menu menu = new Gtk.Menu();
+            private void RightClickMenu(object obj, EventArgs args) {
+                Gtk.Menu menu = new Gtk.Menu();
 
-                 // Add Button
-                 ImageMenuItem add_button = new ImageMenuItem("Add New Feed");
-                 Gtk.Image add_button_img = new Gtk.Image(Gtk.Stock.Add, Gtk.IconSize.Menu);
-                 add_button.Image = add_button_img;
-                 menu.Append(add_button);
-                 
-                 // Refresh Button
-                 ImageMenuItem refresh_button = new ImageMenuItem("Refresh Feeds");
-                 Gtk.Image refresh_button_img = new Gtk.Image(Gtk.Stock.Refresh, Gtk.IconSize.Menu);
-                 refresh_button.Image = refresh_button_img;
-                 menu.Append(refresh_button);
+                // Add Button
+                ImageMenuItem add_button = new ImageMenuItem("Add New Feed");
+                Gtk.Image add_button_img = new Gtk.Image(Gtk.Stock.Add, Gtk.IconSize.Menu);
+                add_button.Image = add_button_img;
+                menu.Append(add_button);
+                
+                // Refresh Button
+                ImageMenuItem refresh_button = new ImageMenuItem("Refresh Feeds");
+                Gtk.Image refresh_button_img = new Gtk.Image(Gtk.Stock.Refresh, Gtk.IconSize.Menu);
+                refresh_button.Image = refresh_button_img;
+                menu.Append(refresh_button);
 
-                 // Preferences Button
-                 ImageMenuItem prefs_button = new ImageMenuItem("Preferences");
-                 Gtk.Image prefs_button_img = new Gtk.Image(Gtk.Stock.Preferences, Gtk.IconSize.Menu);
-                 prefs_button.Image = prefs_button_img;
-                 menu.Append(prefs_button);
+                // Preferences Button
+                ImageMenuItem prefs_button = new ImageMenuItem("Preferences");
+                Gtk.Image prefs_button_img = new Gtk.Image(Gtk.Stock.Preferences, Gtk.IconSize.Menu);
+                prefs_button.Image = prefs_button_img;
+                menu.Append(prefs_button);
+                
+                // Show/hide Button
+                ImageMenuItem show_button;
+                if ( shown ) {
+                    show_button = new ImageMenuItem("Hide the window");
+                } else {
+                    show_button = new ImageMenuItem("Show the window");
+                }
+                menu.Append(show_button);
 
-                 // Quit Button
-                 ImageMenuItem quit_button = new ImageMenuItem("Quit");
-                 Gtk.Image quit_button_img = new Gtk.Image(Gtk.Stock.Quit, Gtk.IconSize.Menu);
-                 quit_button.Image = quit_button_img;
-                 menu.Append(quit_button);
+                // Quit Button
+                ImageMenuItem quit_button = new ImageMenuItem("Quit");
+                Gtk.Image quit_button_img = new Gtk.Image(Gtk.Stock.Quit, Gtk.IconSize.Menu);
+                quit_button.Image = quit_button_img;
+                menu.Append(quit_button);
 
-                 menu.ShowAll();
-                 menu.Popup();
+                menu.ShowAll();
+                menu.Popup();
 
-                 add_button.Activated += new EventHandler(b.ShowAddWindow);
-                 refresh_button.Activated += new EventHandler(b.UpdateAll);
-                 prefs_button.Activated += new EventHandler(b.ShowConfigDialog);
-                 quit_button.Activated += new EventHandler(b.CloseWindow);
-             }
+                add_button.Activated += new EventHandler(b.ShowAddWindow);
+                refresh_button.Activated += new EventHandler(b.UpdateAll);
+                prefs_button.Activated += new EventHandler(b.ShowConfigDialog);
+                show_button.Activated += new EventHandler(ToggleBrowserStatus);
+                quit_button.Activated += new EventHandler(b.CloseWindow);
+            }
         }
     }
 }
