5886502245eccebc432b5611628667eb0c64a84e
Ethan Osten <eosten@Phineas.(none)>
2008-06-13 19:21:36 -0700

    more progress toward a threaded Updater

--- src/Summa/Summa.Core/Updater.cs
+++ src/Summa/Summa.Core/Updater.cs
@@ -7,12 +7,13 @@ using GLib;
 namespace Summa {
     namespace Core {
         public class Updater {
-            private bool updating;
+            public bool Updating;
             private bool should_automatic_update;
+            private bool enqueued;
             
             private Queue updating_queue;
             
-            public ArrayList Updating {
+            public ArrayList FeedsUpdating {
                 get {
                     Summa.Data.Feed[] array = (Summa.Data.Feed[])updating_queue.ToArray();
                     ArrayList retval = new ArrayList();
@@ -26,48 +27,49 @@ namespace Summa {
             }
             
             public Updater() {
-                updating = false;
+                Updating = false;
                 should_automatic_update = true;
+                enqueued = false;
                 
                 updating_queue = new Queue(20);
-                
-                System.Threading.Thread updatethread = new System.Threading.Thread(UpdateThread);
-                updatethread.Start();
                 
                 GLib.Timeout.Add(Summa.Core.Config.GlobalUpdateInterval, new GLib.TimeoutHandler(ScheduledUpdate));
             }
             
             private void UpdateThread() {
-                while ( true ) {
-                    if ( updating_queue.Count != 0 ) {
-                        updating = true;
-                        
-                        Summa.Data.Feed feed = (Summa.Data.Feed)updating_queue.Dequeue();
-                        bool update = feed.Update();
-                        
-                        // it should enter the main loop here long enough to refresh the FeedView and ItemView if necessary.
-                        System.Console.WriteLine(feed.Name+" has "+update.ToString());
-                    } else {
-                        updating = false;
-                    }
+                while ( updating_queue.Count != 0 ) {
+                    System.Console.WriteLine(Updating);
+                    
+                    Summa.Data.Feed feed = (Summa.Data.Feed)updating_queue.Dequeue();
+                    Updating = true;
+                    bool update = feed.Update();
+                    
+                    // it should enter the main loop here long enough to refresh the FeedView and ItemView if necessary.
+                    System.Console.WriteLine(feed.Name+" has "+update.ToString());
                 }
             }
             
             public void Update() {
-                if (!updating) {
+                if (!Updating) {
                     should_automatic_update = false;
                     
                     foreach ( Summa.Data.Feed feed in Summa.Data.Core.GetFeeds() ) {
                         updating_queue.Enqueue(feed);
                     }
+                
+                    System.Threading.Thread updatethread = new System.Threading.Thread(UpdateThread);
+                    updatethread.Start();
                 }
             }
             
             public bool ScheduledUpdate() {
-                if ( !updating && should_automatic_update ) {
+                if ( !Updating && should_automatic_update ) {
                     foreach ( Summa.Data.Feed feed in Summa.Data.Core.GetFeeds() ) {
                         updating_queue.Enqueue(feed);
                     }
+                
+                    System.Threading.Thread updatethread = new System.Threading.Thread(UpdateThread);
+                    updatethread.Start();
                     
                     return true;
                 } else {
