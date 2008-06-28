02e11b4bb294814452e308e05191a16495ffca6e
Will Farrington <wcfarrington@gmail.com>
2008-06-28 19:57:25 -0400

    Re-license to MIT

--- src/Summa/Summa.Gui/TagView.cs
+++ src/Summa/Summa.Gui/TagView.cs
@@ -1,24 +1,27 @@
-///* /home/eosten/Summa/Summa/TagView.cs
-// *
-// * Copyright (C) 2008  Ethan Osten
-// *
-// * This library is free software: you can redistribute it and/or modify
-// * it under the terms of the GNU Lesser General Public License as published by
-// * the Free Software Foundation, either version 2.1 of the License, or
-// * (at your option) any later version.
-// *
-// * This library is distributed in the hope that it will be useful,
-// * but WITHOUT ANY WARRANTY; without even the implied warranty of
-// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// * GNU Lesser General Public License for more details.
-// *
-// * You should have received a copy of the GNU Lesser General Public License
-// * along with this library.  If not, see <http://www.gnu.org/licenses/>.
-// *
-// * Author:
-// *     Ethan Osten <senoki@gmail.com>
-// */
+// TagView.cs
 //
+// Copyright (c) 2008 Ethan Osten
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
 
 using System;
 using System.Collections;
@@ -66,7 +69,7 @@ namespace Summa {
                 if ( Summa.Core.Application.Browsers.Count == 0 ) {
                     Gtk.TreeIter tagiter;
                     tagiter = Summa.Core.Application.TagStore.Append();
-                    Pixbuf icon = new Gdk.Pixbuf("/usr/share/epiphany-browser/icons/hicolor/16x16/status/feed-presence.png");
+                    Pixbuf icon = new Gdk.Pixbuf("/Users/wfarr/Desktop/feed-presence.png");
                     Summa.Core.Application.TagStore.SetValue(tagiter, 0, icon);
                     Summa.Core.Application.TagStore.SetValue(tagiter, 1, "All feeds");
                 
@@ -94,7 +97,7 @@ namespace Summa {
             public void AppendTag(string tag) {
                 Gtk.TreeIter iter;
                 iter = Summa.Core.Application.TagStore.Append();
-                Summa.Core.Application.TagStore.SetValue(iter, 0, new Gdk.Pixbuf("/home/eosten/Desktop/tag.png"));
+                Summa.Core.Application.TagStore.SetValue(iter, 0, new Gdk.Pixbuf("/Users/wfarr/Desktop/tag.png"));
                 Summa.Core.Application.TagStore.SetValue(iter, 1, tag);
             }
         }
