947fb380dccaf3cdf4a40e43b53b0679659dbdfa
Ethan Osten <eosten@Phineas.(none)>
2008-07-01 23:03:12 -0700

    add a regular expression to fix a common HTML malformation.

--- src/Summa/Summa.Net/Request.cs
+++ src/Summa/Summa.Net/Request.cs
@@ -35,10 +35,41 @@ namespace Summa {
             
             public HttpStatusCode Status;
             
-            public string Etag;
-            public string LastModified;
+            private string etag;
+            public string Etag {
+                get {
+                    try {
+                        return etag;
+                    } catch ( Exception e ) {
+                        return "";
+                    }
+                }
+                set { etag = value; }
+            }
             
-            public string Xml;
+            private string lastmodified;
+            public string LastModified {
+                get {
+                    try {
+                        return lastmodified;
+                    } catch ( Exception e ) {
+                        return "";
+                    }
+                }
+                set { lastmodified = value; }
+            }
+            
+            private string xml;
+            public string Xml {
+                get {
+                    try {
+                        return xml;
+                    } catch ( Exception e ) {
+                        return "";
+                    }
+                }
+                set { xml = value; }
+            }
             
             private HttpWebRequest httprequest;
             private HttpWebResponse httpresponse;
@@ -51,49 +82,52 @@ namespace Summa {
                     httprequest = (HttpWebRequest)WebRequest.Create(uri);
                     httprequest.AllowAutoRedirect = true;
                     
-                    try {
+                    /*try {
                         string modified = Summa.Core.Application.Database.GetFeed(uri)[9];
                         DateTime m = Convert.ToDateTime(modified);
                         httprequest.IfModifiedSince = m;
-                    } catch ( Exception e ) {}
+                    } catch ( Exception e ) {}*/
                     
                     httpresponse = (HttpWebResponse)httprequest.GetResponse();
-            		Status = httpresponse.StatusCode;
+                    Status = httpresponse.StatusCode;
                     
-            		byte[] buffer = new byte[8192];
-            		StringBuilder sb  = new StringBuilder();
+                    byte[] buffer = new byte[8192];
+                    StringBuilder sb  = new StringBuilder();
                     
                     if ( httpresponse.StatusCode == HttpStatusCode.NotModified ) {
                         Xml = "";
                     } else {
-                		stream = httpresponse.GetResponseStream();
-                		string tempString = null;
-                		int count = 0;
-                		
-                		do {
-                			count = stream.Read(buffer, 0, buffer.Length);
-                            
-                			if (count != 0) {
-                				tempString = Encoding.ASCII.GetString(buffer, 0, count);
-                                
-                				sb.Append(tempString);
-                			}
-                		}
-                		while (count > 0);
+                        stream = httpresponse.GetResponseStream();
+                        string tempString = null;
+                        int count = 0;
                         
-                		Xml = sb.ToString();
-                		LastModified = httpresponse.LastModified.ToString();
-                		try {
-                    		Etag = httpresponse.Headers.GetValues("ETag")[0];
-                		} catch ( Exception e ) {
-                		    Summa.Core.Util.Log("Etag not found", e);
-            		    }
-        		    }
-    		    } catch ( System.Net.WebException e ) {
-    		        Summa.Core.Util.Log("Failed to download", e);
-    		        Status = HttpStatusCode.NotFound;
-    		        return;
-		        }
+                        do {
+                            count = stream.Read(buffer, 0, buffer.Length);
+                            
+                            if (count != 0) {
+                                tempString = Encoding.ASCII.GetString(buffer, 0, count);
+                                
+                                sb.Append(tempString);
+                            }
+                        }
+                        while (count > 0);
+                        
+                        Xml = sb.ToString();
+                        Xml = System.Text.RegularExpressions.Regex.Replace(Xml, "( [a-z]+)=([a-zA-Z0-9:/._%;?=&-]+)", "$1=\"$2\"");
+                        Console.WriteLine(Xml);
+                        
+                        LastModified = httpresponse.LastModified.ToString();
+                        try {
+                            Etag = httpresponse.Headers.GetValues("ETag")[0];
+                        } catch ( Exception e ) {
+                            Summa.Core.Util.Log("Etag not found", e);
+                        }
+                    }
+                } catch ( System.Net.WebException e ) {
+                    Summa.Core.Util.Log("Failed to download", e);
+                    Status = HttpStatusCode.NotFound;
+                    return;
+                }
             }
         }
     }
